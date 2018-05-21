aaac17a9757747ffc730deae46fb96ff02fca8d1
Gabriel Cossette <gabriel@gcossette.com>
2018-05-20 22:55:28 -0400

    Update

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'CONFIG_PASS'
+
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ] ; then
+    cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
+<?php
+\$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
+EOT
+fi
+
+if [ ! -f /etc/phpmyadmin/config.user.inc.php ] ; then
+cat > /etc/phpmyadmin/config.user.inc.php <<EOF
+	<?php
+	$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+	$cfg['Servers'][$i]['controluser'] = 'phpmyadmin';
+	$cfg['Servers'][$i]['controlpass'] = '${CONFIG_PASS}';
+	?>
+EOF
+fi
+
+mkdir -p /var/nginx/client_body_temp
+chown nobody:nobody /sessions /var/nginx/client_body_temp
+mkdir -p /var/run/php/
+chown nobody:nobody /var/run/php/
+touch /var/log/php-fpm.log
+chown nobody:nobody /var/log/php-fpm.log
+
+if [ "$1" = 'phpmyadmin' ]; then
+    exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
+fi
