d6c5b87a5f22248285be0026bce2eb41a759b76a
Gabriel Cossette <gabriel@gcossette.com>
2018-07-01 16:52:30 -0400

    Update

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ file_env() {
 
 file_env 'CONFIG_PASS'
 
-if [ ! -f /etc/phpmyadmin/config.secret.inc.php ] ; then
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
     cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
 <?php
 \$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
@@ -38,12 +38,10 @@ cat > /etc/phpmyadmin/config.user.inc.php <<EOF
 EOF
 
 mkdir -p /var/nginx/client_body_temp
-chown nobody:nobody /sessions /var/nginx/client_body_temp
+chown nobody:nogroup /sessions /var/nginx/client_body_temp
 mkdir -p /var/run/php/
-chown nobody:nobody /var/run/php/
+chown nobody:nogroup /var/run/php/
 touch /var/log/php-fpm.log
-chown nobody:nobody /var/log/php-fpm.log
+chown nobody:nogroup /var/log/php-fpm.log
 
-if [ "$1" = 'phpmyadmin' ]; then
-    exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
-fi
+exec "$@"
